1ac2ce73380fdca8831632ce6b033f8a027747ce
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-04-01 14:26:50 -0700

    parameter change

--- CIncrement/108/code.cpp
+++ CIncrement/108/code.cpp
@@ -12,12 +12,12 @@
 class Solution {
 public:
     TreeNode *addNodes(const vector<int> &nums, int left, int right) {
-        if (left >= right) {
+        if (left > right) {
             return nullptr;
         }
         int middle = (left + right) / 2;
         TreeNode *nNode = new TreeNode(nums[middle]);
-        nNode->left = addNodes(nums, left, middle);
+        nNode->left = addNodes(nums, left, middle - 1);
         nNode->right = addNodes(nums, middle + 1, right);
         return nNode;
     }
@@ -26,7 +26,7 @@ public:
             TreeNode *n = new TreeNode(nums[0]);
             return n;
         }
-        TreeNode *root = addNodes(nums, 0, nums.size());
+        TreeNode *root = addNodes(nums, 0, nums.size() - 1);
         return root;
     }
 };
